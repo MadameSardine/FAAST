981e5f1f880cab71761961964233b04b179b3f6d
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-22 12:13:36 +0100

    Using doubles for station

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,25 +1,17 @@
 require './lib/station'
-require './lib/train'
 
 describe Station do
 
 	let(:station) {Station.new}
-	let(:train) {Train.new}
-	let(:user) {User.new}
+	let(:train) {double :train}
+	let(:user) {double :user}
 	
 	it "has no train" do
 		expect(station.train_count).to eq(0)
 	end
 
 	it "can allow a train in" do
-		station.allow_in(train)
-		expect(station.train_count).to eq(1)
-	end
-
-	it "can allow several trains in" do
-		trains = [Train.new, Train.new]
-		trains.each{|train| station.allow_in(train)}
-		expect(station.train_count).to eq(2)
+		expect{station.allow_in(train)}.to change{station.train_count}.by 1
 	end
 
 	it "can allow a train to stop in the station" do
